7a2b90ab1ed259c12617aed0b68db395c73bce48
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-05-19 00:11:07 +0900

    feat: sitemap 수정

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,3 +1,13 @@
 module.export = {
-  plugins: [["sitemap", { hostname: "https://main--kaleidoscopic-daffodil-c4b63e.netlify.app/#/" }]],
+  plugins: [
+    [
+      "sitemap",
+      {
+        hostname: "https://main--kaleidoscopic-daffodil-c4b63e.netlify.app/#/",
+        dateFormatter: val => {
+          return new Date().toISOString();
+        },
+      },
+    ],
+  ],
 };
